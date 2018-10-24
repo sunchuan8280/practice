1dd343bac0cc211f9a14a0003f49a28264798360
sunchuan <sunchuan8280@163.com>
2018-10-24 22:59:30 +0800

    2018-10-24 22:59

---
+++ src/main/java/coding/MyStack1.java
@@ -0,0 +1,59 @@
+package coding;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Stack;
+
+/**
+ * Created by chuan on 2018/10/24.
+ */
+public class MyStack1 {
+    private Stack<Integer> stackData;
+    private Stack<Integer> stackMin;
+
+    public MyStack1(){
+        stackData=new Stack<>();
+        stackMin=new Stack<>();
+    }
+
+    public void push(int newNum){
+        stackData.push(newNum);
+        if(stackMin.isEmpty()){
+            stackMin.push(newNum);
+        }else{
+            if(newNum<=getMin()){
+                stackMin.push(newNum);
+            }
+        }
+    }
+    public int pop(){
+        int value=stackData.pop();
+        if(value==getMin()){
+            this.stackMin.pop();
+        }
+        return value;
+    }
+
+    public int getMin(){
+        return stackMin.peek();
+    }
+    @Test
+    public void test(){
+        MyStack1 stack1=new MyStack1();
+        stack1.push(6);
+        stack1.push(2);
+        stack1.push(1);
+        stack1.push(5);
+        stack1.push(4);
+        stack1.push(3);
+
+        Assert.assertSame(1,stack1.getMin());
+        stack1.pop();
+        stack1.pop();
+        stack1.pop();
+        stack1.pop();
+        stack1.pop();
+        Assert.assertSame(6,stack1.getMin());
+    }
+}
---
+++ src/main/java/coding/MyStack2.java
@@ -0,0 +1,62 @@
+package coding;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Stack;
+
+/**
+ * Created by chuan on 2018/10/24.
+ */
+public class MyStack2 {
+    private Stack<Integer> stackData;
+    private Stack<Integer> stackMin;
+
+    public MyStack2(){
+        stackData=new Stack<>();
+        stackMin=new Stack<>();
+    }
+
+    public void push(int newNum){
+        stackData.push(newNum);
+        if(stackMin.isEmpty()){
+            stackMin.push(newNum);
+        }else{
+            stackMin.push(newNum<=getMin()?newNum:getMin());
+        }
+    }
+    public int pop(){
+        if(this.stackData.isEmpty()){
+            throw new RuntimeException("Your stack is empty");
+        }
+        int value=stackData.pop();
+        this.stackMin.pop();
+        return value;
+    }
+
+    public int getMin(){
+        if(this.stackMin.isEmpty()){
+            throw new RuntimeException("Your stack is empty");
+        }
+        return stackMin.peek();
+    }
+
+    @Test
+    public void test(){
+        MyStack2 stack2=new MyStack2();
+        stack2.push(6);
+        stack2.push(2);
+        stack2.push(1);
+        stack2.push(5);
+        stack2.push(4);
+        stack2.push(3);
+
+        Assert.assertSame(1,stack2.getMin());
+        stack2.pop();
+        stack2.pop();
+        stack2.pop();
+        stack2.pop();
+        stack2.pop();
+        Assert.assertSame(6,stack2.getMin());
+    }
+}
