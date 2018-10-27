953b2334070ee34e779f5767ba949eca61356be3
sunchuan <sunchuan8280@163.com>
2018-10-27 23:05:48 +0800

    sort stack by another stack

---
+++ src/main/java/coding/StackSort.java
@@ -0,0 +1,32 @@
+package coding;
+
+import org.junit.Test;
+
+import java.util.Stack;
+
+/**
+ * Created by chuan on 2018/10/27.
+ */
+public class StackSort {
+    @Test
+    public  void stackSorttest(){
+         Stack<Integer> stack = new Stack<>();
+         Stack<Integer> help = new Stack<>();
+         stack.push(6);
+         stack.push(1);
+         stack.push(3);
+         stack.push(2);
+         stack.push(7);
+         while(!stack.isEmpty()){
+            int cur = stack.pop();
+            while(!help.isEmpty()&&cur < help.peek()){
+                  stack.push(help.pop());
+            }
+            help.push(cur);
+         }
+        while(!help.isEmpty()){
+            stack.push(help.pop());
+        }
+        System.out.println(stack.toString());
+    }
+}
