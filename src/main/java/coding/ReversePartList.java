0af4d0f2f331d422adb1e43b5cf591f716706f53
sunchuan <sunchuan8280@163.com>
2018-11-13 23:32:24 +0800

    20181113

--- src/main/java/coding/ReversePartList.java
+++ src/main/java/coding/ReversePartList.java
@@ -1,5 +1,7 @@
 package coding;
 
+import org.junit.Test;
+
 /**
  * Created by chuan on 2018/11/12.
  */
@@ -8,39 +10,62 @@ public class ReversePartList {
         int len=0;
         Node fpre=null;
         Node tPos=null;
-        Node node1=head;
-        while(node1!=null){
+        Node node=head;
+        // 先找到需要翻转的前一个节点和后一个节点
+        while(node!=null){
             len++;
             if(len==from-1){
-                fpre=node1;
+                fpre=node;
             }
             if(len==to+1){
-                tPos=node1;
+                tPos=node;
             }
-            node1=node1.next;
+            node=node.next;
         }
+
         if(from<1||from>to||to>len){
             return head;
         }
+        //判断是否翻转里有没有头节点，fre==null有
         if(fpre==null){
-            node1=head;
+            node=head;
         }else{
-            node1=fpre.next;
+            node=fpre.next;
         }
-        Node node2=node1.next;
-        node1.next=tPos;
+        Node tmp=node;
+        Node node2=node.next;
         Node next=null;
         while (node2!=tPos){
             next=node2.next;
-            node2.next=node1;
-            node1=node2;
+            node2.next=node;
+            node=node2;
             node2=next;
         }
-        if(fpre!=null){
-            fpre.next=node1;
-            return head;
-        }
-        return node1;
-    }
+        // 链接
+        tmp.next=tPos;
 
+        if(fpre!=null){
+            fpre.next=node;
+            return head;
+        }else{
+            return node;
+        }
+
+
+    }
+    @Test
+    public void test(){
+        Node head1=null;
+
+        Node node1= new Node(1);
+        Node node2= new Node(2);
+        Node node3= new Node(3);
+        Node node4= new Node(4);
+        head1=node1;
+        node1.next=node2;
+        node1.next.next=node3;
+        node1.next.next.next=node4;
+        ReversePartList reversePartList=new ReversePartList();
+        Node head= reversePartList.reversePart(head1,2,3);
+    }
 }
