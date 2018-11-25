0d3244b10477b2bd28555b7fbb83906017e5ad1b
sunchuan <sunchuan8280@163.com>
2018-11-25 12:24:29 +0800

    20181125

--- src/main/java/coding/AddLists.java
+++ src/main/java/coding/AddLists.java
@@ -7,7 +7,12 @@ import java.util.Stack;
  * Created by chuan on 2018/11/24.
  */
 public class AddLists {
-
+    /**
+     * 时间和空间复杂度都是O(n)
+     * @param head1
+     * @param head2
+     * @return
+     */
     public Node addLists1(Node head1,Node head2){
         Stack<Integer> s1=new Stack<>();
         Stack<Integer> s2=new Stack<>();
@@ -41,5 +46,50 @@ public class AddLists {
         }
         return node;
 
+    }
+
+    public Node addLists1(Node head1,Node head2){
+        head1=reverseList(head1);
+        head2=reverseList(head2);
+        int ca=0;
+        int n1=0;
+        int n2=0;
+        int n=0;
+        Node c1=head1;
+        Node c2=head2;
+        Node pre=null;
+        Node node=null;
+        while (c1!=null||c2!=null){
+            n1=c1==null?0:c1.value;
+            n2=c2==null?0:c2.value;
+
+            n=n1+n2+ca;
+            ca=n/10;
+            pre=node;
+            node=new Node(n%10);
+            node.next=pre;
+            c1=c1==null?null:c1.next;
+            c2=c2==null?null:c2.next;
+        }
+        if(ca==1){
+            pre=node;
+            node=new Node(1);
+            node.next=pre;
+        }
+
+        reverseList(head1);
+        reverseList(head2);
+        return node;
+    }
+    public Node reverseList(Node head){
+        Node pre = null;
+        Node next = null;
+        while(head!=null){
+            next=head.next;
+            head.next=pre;
+            pre=head;
+            head=next;
+        }
+        return pre;
     }
 }
