ece0ca71214971af9347614b97d0fce0ab5c4372
sunchuan <sunchuan8280@163.com>
2018-12-04 00:06:54 +0800

    20191204

--- src/main/java/coding/IntersectNode.java
+++ src/main/java/coding/IntersectNode.java
@@ -24,4 +24,54 @@ public class IntersectNode {
         }
         return n1;
     }
+
+    /**
+     * 两个无环的链表
+     * @param head1
+     * @param head2
+     * @return
+     */
+    public Node noLoop(Node head1,Node head2){
+        if(head1==null||head2==null){
+            return null;
+        }
+        Node cur1=head1;
+        Node cur2=head2;
+        int len1=0;
+        int len2=0;
+        while(cur1.next!=null){
+            len1++;
+            cur1=cur1.next;
+        }
+        while(cur2.next!=null){
+            len2++;
+            cur2=cur2.next;
+        }
+
+        if(cur1!=cur2){
+            return null;
+        }
+        // 如果尾节点相同 必定存在环
+        cur1=head1;
+        cur2=head2;
+        if(len1>len2){
+            int n=len1-len2;
+            while(n>0){
+                n--;
+                cur1=cur1.next;
+            }
+        }
+        if(len1<len2){
+            int n=len2-len1;
+            while(n>0){
+                n--;
+                cur2=cur2.next;
+            }
+        }
+        while(cur1!=cur2){
+            cur1=cur1.next;
+            cur2=cur2.next;
+        }
+        return cur1;
+    }
 }
