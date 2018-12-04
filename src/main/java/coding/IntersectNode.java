a2323547c8df5b7e9d74175f330c58374858c91c
sunchuan <sunchuan8280@163.com>
2018-12-04 22:44:28 +0800

    20181204

--- src/main/java/coding/IntersectNode.java
+++ src/main/java/coding/IntersectNode.java
@@ -74,4 +74,62 @@ public class IntersectNode {
         }
         return cur1;
     }
+    public Node bothLoop(Node head1,Node head2,Node loop1,Node loop2){
+        if(loop1==loop2){
+            Node cur1=head1;
+            Node cur2=head2;
+            int n=0;
+            while(cur1!=loop1){
+                n++;
+                cur1=cur1.next;
+            }
+            while (cur2!=loop2){
+                n--;
+                cur2=cur2.next;
+            }
+
+            cur1=n>0?head1:head2;
+            cur2=cur1==head1?head2:head1;
+            n=Math.abs(n);
+            while(n!=0){
+                n--;
+                cur1=cur1.next;
+            }
+
+            while(cur1!=cur2){
+                cur1=cur1.next;
+                cur2=cur2.next;
+            }
+            return cur1;
+        }else{
+            Node cur1=loop1.next;
+             while(cur1!=loop1){
+                 if(cur1==loop2){
+                     // loop1 loop2 都可返回
+                     return loop1;
+                 }
+                 cur1=cur1.next;
+             }
+             return null;
+        }
+    }
+
+    public Node getIntersectNode(Node head1,Node head2){
+        if(head1==null|| head2==null){
+            return null;
+        }
+
+        Node loop1=getLoopNode(head1);
+        Node loop2=getLoopNode(head2);
+        //都无环
+        if(loop1==null&&loop2==null){
+            return noLoop(head1,head2);
+        }
+        // 都有环
+        if(loop1!=null&&loop2!=null){
+            return bothLoop(head1,head2,loop1,loop2);
+        }
+        // 一个有一个没有，肯定不相交
+        return null;
+    }
 }
